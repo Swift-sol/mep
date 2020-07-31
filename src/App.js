60c7bd6a733fd458a82b2f9ca90cbe466457be56
manuelalferez <manuelalferezruiz@gmail.com>
2020-07-31 20:10:49 +0200

    feat: Adding Container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
 
 function App() {
   return (
     <div className="App">
-      mep
+       <CssBaseline />
+      <Container maxWidth="sm">
+        mep
+      </Container>
     </div>
   );
 }
